7ec3c5715a3d63b5471b20dd6ce16d607f484a15
horothesun <horothesun@gmail.com>
2020-10-24 16:19:38 +0100

    ConcurrentDictionary dependency version bumped

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/horothesun/ConcurrentDictionary",
-            .upToNextMinor(from: "0.1.0")
+            .upToNextMinor(from: "0.1.1")
         )
     ],
     targets: [
